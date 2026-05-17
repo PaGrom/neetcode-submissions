76f5387d208100c45b0963e27ea4ff12a1aedcdd
Pavel Gromov <pagrom23@gmail.com>
2026-05-17 12:48:01 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) {
+            return false;
+        }
+
+        var dict = new Dictionary <char, int>();
+
+        for (int i = 0; i < s.Length; i++) {
+            if (dict.TryGetValue(s[i], out var _)){
+                dict[s[i]]++;
+            }
+            else {
+                dict[s[i]] = 1;
+            }
+
+            if (dict.TryGetValue(t[i], out var _)){
+                dict[t[i]]--;
+            }
+            else {
+                dict[t[i]] = -1;
+            }
+        }
+
+        return !dict.Values.Any(v => v != 0);
+    }
+}
